6bf8c780834c4d16706ea1790afbb404ae7c6b5c
dogdong <yxd5126@gmail.com>
2016-11-08 10:34:51 +0800

    项目初始化

---
+++ YxdDouyu/YxdDouyu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YxdDouyu
+//
+//  Created by tuanshang on 16/11/8.
+//  Copyright © 2016年 tuanshang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YxdDouyu/YxdDouyu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YxdDouyu
+//
+//  Created by tuanshang on 16/11/8.
+//  Copyright © 2016年 tuanshang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
